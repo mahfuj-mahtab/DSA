9765f6e9272ffbf100d51519a0ecd4d63aa2c09d
Mahfuj Mahtab Mohot <mohotmohot1@gmail.com>
2023-05-03 05:39:59 -0400

    working on 2nd largest number

--- Problem/Array/SecondLargest.py
+++ Problem/Array/SecondLargest.py
@@ -1,2 +1,20 @@
 # solution in Bubble Sort
 arr = [int(x) for x in input().strip().split()]
+
+for i in range(len(arr)):
+    for j in range(0,len(arr)-1):
+        if(arr[i] < arr[j]):
+            arr[j],arr[i] = arr[i],arr[j]
+
+
+c = 0
+small = 0
+for i in range(len(arr)-1,-1,-1):
+    if(arr[-1] > arr[i]):
+        c+=1
+        small = arr[i]
+        break
+if(c == 0):
+    print(-1)
+else:
+    print(small)
